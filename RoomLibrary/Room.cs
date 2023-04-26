485e7ffec3990833dfe08ab5f007d4c37eb39814
kamillatalibova18 <118733931+kamillatalibova18@users.noreply.github.com>
2023-04-26 12:36:25 +0300

    Add files via upload

---
+++ RoomLibrary/Room.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class Room
+    {
+        double roomLength;//длина комнаты
+        public double roomWidth;//ширина комнаты
+
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        /// <summary>
+        /// Метод вычисляет периметр комнаты
+        /// </summary>
+        /// <returns>Вщзвращает значение периметра </returns>
+        public double RoomPerimetr()
+        {
+            return 2 * (roomLength + RoomWidth);
+        }
+        /// <summary>
+        /// Метод вычисляет площадь комнаты
+        /// </summary>
+        /// <returns>Вщзвращает значение площади </returns>
+        
+        public double RoomArea()
+        {
+            return roomLength * RoomWidth;
+        }
+
+        ///<summary>
+        /// Метод вычисляет число квадратных метров
+        /// на одного человека
+        /// </summary>
+        /// <param name="np">Число человек</param>
+        /// <returns>Вщзвращает число квадратных метров </returns>
+        
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+
+
+
+    }
+}
